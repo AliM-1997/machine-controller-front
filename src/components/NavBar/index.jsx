c6e7e2e7a51284a46afcdf07ad7caf007873c0c5
AliM-1997 <aliimortada797@gmail.com>
2024-08-24 18:48:33 +0300

    setting the static top shape for the nav bar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import logo from "../../assets/images/logo.png";
 import "./style.css";
+import Button from "../../base/Button";
 const NavBar = () => {
   return (
-    <div className="flex nav-container">
-      <div className="flex center logo">
-        <img src={logo} alt="Logo" className="logo" />
-        <h1>
-          <span className="highlight">D</span>ustry
-        </h1>
+    <div className="flex column nav-container white-bg full-width full-height">
+      <div className="top-nav">
+        <div className="flex center logo">
+          <img src={logo} alt="Logo" className="logo" />
+          <h1>
+            <span className="highlight">D</span>ustry
+          </h1>
+        </div>
+        <div className="flex column center gap">
+          <Button placeHolder="DashBoard" width="250px" />
+          <Button placeHolder="Machines" width="250px" />
+          <Button placeHolder="Tasks" width="250px" />
+          <Button placeHolder="Predictions" width="250px" />
+          <Button placeHolder="Alerts" width="250px" />
+          <Button placeHolder="User Management" width="250px" />
+        </div>
       </div>
-      <div></div>
+
       <div></div>
     </div>
   );
