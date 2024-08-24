8b02e97d78a2f893d5b7ff3235c8fe2e8713fb45
AliM-1997 <aliimortada797@gmail.com>
2024-08-24 21:00:20 +0300

    adding signup route to app file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./styles/utilities.css";
 import "./styles/index.css";
 import Layout from "./components/Layout";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}></Route>
           <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </div>
