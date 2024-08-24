1ff680079e8e208919747042c4cc9fd2ead62f61
AliM-1997 <aliimortada797@gmail.com>
2024-08-24 18:14:50 +0300

    adding the logo to the side nav bar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import logo from "../../assets/images/logo.png";
+import "./style.css";
 const NavBar = () => {
-  return <div>hello from nav bar</div>;
+  return (
+    <div className="flex nav-container">
+      <div className="flex center logo">
+        <img src={logo} alt="Logo" className="logo" />
+        <h1>
+          <span className="highlight">D</span>ustry
+        </h1>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default NavBar;
