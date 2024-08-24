20c03ddbb9563193bdfd1f1a60dfb779442743a3
AliM-1997 <aliimortada797@gmail.com>
2024-08-24 15:16:12 +0300

    adding the layout structure component

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import NavBar from "../NavBar";
+import Header from "../Header";
 import "./style.css";
+import { Outlet } from "react-router-dom";
 const Layout = () => {
-  return <div>hello from layout</div>;
+  return (
+    <div>
+      <div className="flex row page ">
+        <NavBar />
+        <div className="flex column">
+          <Header />
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
