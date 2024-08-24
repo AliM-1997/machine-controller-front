b32a3350fe5e86e35f4ca5e69bf2430efb5511f7
AliM-1997 <aliimortada797@gmail.com>
2024-08-24 20:19:45 +0300

    adding static structure for login container

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import "./style.css";
+import Input from "../../base/Input";
+import Button from "../../base/Button";
 
 const Login = () => {
-  return <div>hello from login</div>;
+  return (
+    <div className="page flex center column ">
+      <div className="flex column gap login-container">
+        <Input
+          className=""
+          name="Email"
+          width="400px"
+          type="email"
+          placeHolder="examle@gmail.com"
+        />
+        <Input
+          name="Password"
+          width="full-width"
+          type="password"
+          placeHolder="password"
+        />
+        <Button
+          backgroundColor="primary"
+          width="400px"
+          textColor="White"
+          placeHolder="LogIn"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
