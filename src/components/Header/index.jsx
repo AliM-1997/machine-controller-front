0a869a11397c0f744c16a29a1567edabbbf06cb8
AliM-1997 <aliimortada797@gmail.com>
2024-08-24 17:40:20 +0300

    adding the input and icons to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 import "./style.css";
-const Header = () => {
-  return <div>hello from header</div>;
+import Input from "../../base/Input";
+import {
+  faSearch,
+  faList,
+  faUser,
+  faBell,
+} from "@fortawesome/free-solid-svg-icons";
+import Icon from "../../base/Icon";
+
+const Header = ({ pagename }) => {
+  return (
+    <div className="flex center header white-bg">
+      <div>
+        <Input width="300px" leftIcon={faSearch} rightIcon={faList} />
+      </div>
+
+      <div className="flex center h-icon">
+        <Icon icon={faBell} />
+        <Icon icon={faUser} />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
