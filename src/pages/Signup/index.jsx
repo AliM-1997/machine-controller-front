d1bcb936f0372505fb0befe4aacabaeca631750e
AliM-1997 <aliimortada797@gmail.com>
2024-08-24 21:13:55 +0300

    adding the logo word and the signup button

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -4,7 +4,12 @@ import Input from "../../base/Input";
 import Button from "../../base/Button";
 const Signup = () => {
   return (
-    <div className="flex column center page">
+    <div className="flex column center page gap">
+      <div>
+        <h1>
+          <span className="highlight">D</span>ustry
+        </h1>
+      </div>
       <div className="flex column signup-input ">
         <Input placeHolder="name" width="400px" name="Name" />
         <Input placeHolder="example@gamil.com" width="400px" name="Email" />
@@ -13,6 +18,14 @@ const Signup = () => {
           placeHolder="confirmed-password"
           width="400px"
           name="Confirmed Passowrd"
+        />
+      </div>
+      <div className="flex column gap">
+        <Button
+          backgroundColor="primary"
+          width="400px"
+          textColor="white"
+          placeHolder="Signup"
         />
       </div>
     </div>
