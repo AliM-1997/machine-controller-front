87b4852e769e4bcb08ad119441e26422f7a71b26
AliM-1997 <aliimortada797@gmail.com>
2024-08-24 21:11:47 +0300

    adding the signup inputs

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import "./style.css";
+import Input from "../../base/Input";
+import Button from "../../base/Button";
 const Signup = () => {
-  return <div>hello from Signup</div>;
+  return (
+    <div className="flex column center page">
+      <div className="flex column signup-input ">
+        <Input placeHolder="name" width="400px" name="Name" />
+        <Input placeHolder="example@gamil.com" width="400px" name="Email" />
+        <Input placeHolder="password" width="400px" name="Password" />
+        <Input
+          placeHolder="confirmed-password"
+          width="400px"
+          name="Confirmed Passowrd"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
