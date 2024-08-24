5b6b4237ebc1e0033638055ef9a1ec214905f02f
AliM-1997 <aliimortada797@gmail.com>
2024-08-24 20:55:22 +0300

    finalizing shap of login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -6,26 +6,56 @@ import Button from "../../base/Button";
 const Login = () => {
   return (
     <div className="page flex center column ">
-      <div className="flex column gap login-container">
-        <Input
-          className=""
-          name="Email"
-          width="400px"
-          type="email"
-          placeHolder="examle@gmail.com"
-        />
-        <Input
-          name="Password"
-          width="full-width"
-          type="password"
-          placeHolder="password"
-        />
-        <Button
-          backgroundColor="primary"
-          width="400px"
-          textColor="White"
-          placeHolder="LogIn"
-        />
+      <div className=" flex column center login-container white-bg gap ">
+        <div className=" flex column center gap">
+          <h1>
+            <span className="highlight">D</span>ustry
+          </h1>
+          <div className="switch flex black-bg">
+            <Button
+              placeHolder="Login"
+              backgroundColor="primary"
+              width="125px"
+              textColor="white"
+            />
+            <Button
+              placeHolder="Register"
+              backgroundColor="black"
+              textColor="white"
+              width="125px"
+            />
+          </div>
+        </div>
+        <div className="flex column gap ">
+          <Input
+            className=""
+            name="Email"
+            width="400px"
+            type="email"
+            placeHolder="examle@gmail.com"
+          />
+          <Input
+            name="Password"
+            width="full-width"
+            type="password"
+            placeHolder="password"
+          />
+          <p className="underline">forgetPassword?</p>
+          <Button
+            backgroundColor="primary"
+            width="400px"
+            textColor="White"
+            placeHolder="Login"
+          />
+          <Button
+            className="employee"
+            backgroundColor="secondary"
+            width="400px"
+            textColor="White"
+            placeHolder="switch to employee"
+            border={true}
+          />
+        </div>
       </div>
     </div>
   );
