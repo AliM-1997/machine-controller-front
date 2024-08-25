882a42c3c8aba530685801b1927d7d729907ed6d
AliM-1997 <aliimortada797@gmail.com>
2024-08-25 11:08:01 +0300

    adding dashboard route to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./styles/index.css";
 import Layout from "./components/Layout";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,9 +12,11 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />}></Route>
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="dashboard" element={<Dashboard />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
