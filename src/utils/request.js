1eda2bbc2fd88f1c6f1415c2a01f214d09d26280
AliM-1997 <aliimortada797@gmail.com>
2024-08-25 20:31:00 +0300

    initializing the axios function

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,10 +2,18 @@ import axios from "axios";
 import { RequestMethods } from "./request_methods";
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
-export const requestApi = ({
+export const requestApi = async ({
   includeToken = true,
   route,
   requestMethod = RequestMethods.GET,
   body = null,
   navigationFunction = () => {},
-}) => {};
+}) => {
+  try {
+    const { data } = await axios.request({
+      url: route,
+      method: requestMethod,
+      data: body,
+    });
+  } catch (error) {}
+};
