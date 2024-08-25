da39c9e31f7ebd1eaac3a26578eff4dda58110df
AliM-1997 <aliimortada797@gmail.com>
2024-08-25 23:13:42 +0300

    initializing the get all user fetching to user remote data

--- src/data/remote/User.js
+++ src/data/remote/User.js
@@ -1,6 +1,20 @@
+import { requestApi } from "../../utils/request";
+import { RequestMethods } from "../../utils/request_methods";
+
 export const Users = {
-  GetAllUsers,
-  DeleteUser,
-  EditeUser,
-  CreateUser,
+  GetAllUsers: async () => {
+    try {
+      const { data } = await requestApi({
+        route: "/user",
+        requestMethod: RequestMethods.GET,
+      });
+      return data;
+    } catch (error) {
+      console.error("Failed to get all users:", error);
+      throw error;
+    }
+  },
+  //   DeleteUser,
+  //   EditeUser,
+  //   CreateUser,
 };
