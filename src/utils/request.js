9e093a0342a45382bc803ee6ffd71e9b9fedc9e6
AliM-1997 <aliimortada797@gmail.com>
2024-08-25 20:28:57 +0300

    adding the axios base url

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,7 @@
+import axios from "axios";
 import { RequestMethods } from "./request_methods";
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+
 export const requestApi = ({
   includeToken = true,
   route,
