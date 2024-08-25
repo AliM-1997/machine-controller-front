de3191cd295b30613927e42ba6776a951b685cf0
AliM-1997 <aliimortada797@gmail.com>
2024-08-25 22:34:09 +0300

    adding the static button and search for users page with some styling

--- src/pages/AllUsers/index.jsx
+++ src/pages/AllUsers/index.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import "./style.css";
+import Input from "../../base/Input";
+import Button from "../../base/Button";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 const AllUsers = () => {
   return (
-    <div className="flex column page users-container">
+    <div className="flex column users-container gap">
       <div>
         <h2> All Users</h2>
       </div>
-      <div></div>
+      <div className="flex user-input-container">
+        <Input width="300px" leftIcon={faSearch} />
+        <div className="flex center gap">
+          <Button
+            className="filter-user"
+            width="200px"
+            backgroundColor="primary"
+            placeHolder="add User"
+            textColor="white"
+          />
+          <Button
+            className="add-user"
+            width="120px"
+            backgroundColor="primary"
+            textColor="white"
+            placeHolder="Filter"
+          />
+        </div>
+      </div>
       <div></div>
     </div>
   );
