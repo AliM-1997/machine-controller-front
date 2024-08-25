24e76ad80e578eea3ada398ba41e54dce44a82f5
AliM-1997 <aliimortada797@gmail.com>
2024-08-25 12:18:34 +0300

    adding state to layout to handel changing both the header naname and the page itself

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../NavBar";
 import Header from "../Header";
 import "./style.css";
 import { Outlet } from "react-router-dom";
 const Layout = () => {
+  const [clickedPage, setClickedPage] = useState(null); // Lifted state
+
+  const handlePageChange = (newPageName) => {
+    setClickedPage(newPageName);
+  };
   return (
     <div className="flex row page">
       <div className="container-1">
-        <NavBar />
+        <NavBar onNavigate={handlePageChange} isClick={clickedPage} />
       </div>
       <div className="flex column container-2">
-        <Header />
+        <Header pageName={clickedPage} />
         <Outlet />
       </div>
     </div>
