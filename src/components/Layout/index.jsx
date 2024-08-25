01c2899ea0c02b0658b97e4ca5d9be89bc1c5fc8
AliM-1997 <aliimortada797@gmail.com>
2024-08-25 22:10:18 +0300

    adding a switch to set choose input

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,19 +3,42 @@ import NavBar from "../NavBar";
 import Header from "../Header";
 import "./style.css";
 import { Outlet } from "react-router-dom";
+
 const Layout = () => {
-  const [clickedPage, setClickedPage] = useState(null); // Lifted state
+  const [clickedPage, setClickedPage] = useState(null);
+  const [showChooseInput, setShowChooseInput] = useState(true);
 
   const handlePageChange = (newPageName) => {
     setClickedPage(newPageName);
+
+    switch (newPageName) {
+      case "Dashboard":
+        setShowChooseInput(false);
+        break;
+      case "Machines":
+        setShowChooseInput(true);
+        break;
+      case "Tasks":
+        setShowChooseInput(true);
+        break;
+      case "Predictions":
+        setShowChooseInput(true);
+        break;
+      case "Alerts":
+        setShowChooseInput(false);
+        break;
+      default:
+        setShowChooseInput(true);
+    }
   };
+
   return (
     <div className="flex row page">
       <div className="container-1">
         <NavBar onNavigate={handlePageChange} isClick={clickedPage} />
       </div>
       <div className="flex column container-2">
-        <Header pageName={clickedPage} />
+        <Header pageName={clickedPage} showChooseInput={showChooseInput} />
         <Outlet />
       </div>
     </div>
