0ec6c283565bbf136cfa22ff7159a99e374da0d4
AliM-1997 <aliimortada797@gmail.com>
2024-08-26 20:15:59 +0300

    induce user reduce to store

--- src/data/redux/store.js
+++ src/data/redux/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { userReducer } from "./userSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+  },
 });
 export default store;
