94e15417325c2da5f95f0d15d17567e00989962e
AliM-1997 <aliimortada797@gmail.com>
2024-08-26 12:26:50 +0300

    adiing the name and username static input to user profile

--- src/pages/UserProfile/index.jsx
+++ src/pages/UserProfile/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import Label from "../../base/Label";
+import Input from "../../base/Input";
 const UserProfile = () => {
   return (
     <div className="flex column userProfile-container">
@@ -10,7 +11,10 @@ const UserProfile = () => {
         </h2>
       </div>
       <div></div>
-      <div></div>
+      <div className="flex row center full-width gap">
+        <Input width="35vw" placeHolder="name" name="Name" />
+        <Input width="35vw" placeHolder="@username" name="username" />
+      </div>
       <div></div>
       <div></div>
       <div></div>
