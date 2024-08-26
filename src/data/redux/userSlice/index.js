7bae3280d060895dab0a778f9cac351acdc6a749
AliM-1997 <aliimortada797@gmail.com>
2024-08-26 17:23:11 +0300

    creating user slice file

---
+++ src/data/redux/userSlice/index.js
@@ -0,0 +1,7 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  reducers: {},
+});
+export const {} = userSlice.actions;
+export const userReducer = userSlice.reducer;
