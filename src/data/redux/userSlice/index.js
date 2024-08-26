66d45fb42a23dbccf19e14acf5d4f1e81c575110
AliM-1997 <aliimortada797@gmail.com>
2024-08-26 17:26:34 +0300

    initializing the file structure if the user slice

--- src/data/redux/userSlice/index.js
+++ src/data/redux/userSlice/index.js
@@ -1,7 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+const initialState = {
+  name: "",
+  email: "",
+  username: "",
+  role: "",
+  location: "",
+  phone_number: "",
+};
 const userSlice = createSlice({
-  reducers: {},
+  initialState,
+  name: "user",
+  reducers: {
+    loadUse: (state, action) => {},
+    clearUser: (state, action) => {},
+  },
 });
 export const {} = userSlice.actions;
 export const userReducer = userSlice.reducer;
