80bbea552358fc67ace2b02a1e938bc2b4de5b15
AliM-1997 <aliimortada797@gmail.com>
2024-08-27 00:46:26 +0300

    initializing the image component props

--- src/base/DisplayImage/index.jsx
+++ src/base/DisplayImage/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./style.css";
-const index = () => {
+const DisplayImage = ({
+  url,
+  width = "50px",
+  heigth = "50px",
+  borderRadius = "50%",
+}) => {
+  const baseUrl = process.env.REACT_APP_BASE_URL_IMAGE;
+  console.log("base url", baseUrl);
+  const fullurl = `${baseUrl}/storage/${url}`;
+  console.log("full url", fullurl);
   return <div>hello from image</div>;
 };
 
-export default index;
+export default DisplayImage;
