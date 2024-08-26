f23ac20cdea816eb6ac638e43ce65e2fa9551159
AliM-1997 <aliimortada797@gmail.com>
2024-08-26 12:33:45 +0300

    using the edit and delete compoenent button to user profile

--- src/pages/UserProfile/index.jsx
+++ src/pages/UserProfile/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 import Label from "../../base/Label";
 import Input from "../../base/Input";
+import Button from "../../base/Button";
 const UserProfile = () => {
   return (
     <div className="flex column userProfile-container">
@@ -10,7 +11,23 @@ const UserProfile = () => {
           <Label placeholder="User Profile" fontWeight="bold" />
         </h2>
       </div>
-      <div></div>
+      <div className="flex column center secondary-bg full-width">
+        <div></div>
+        <div className="flex center gap full-width ">
+          <Button
+            placeHolder="Upload image"
+            width="15vw"
+            textColor="white"
+            backgroundColor="primary"
+          />
+          <Button
+            placeHolder="Delete"
+            width="10vw"
+            textColor="white"
+            backgroundColor="primary"
+          />
+        </div>
+      </div>
       <div className="flex row center full-width gap">
         <Input width="35vw" placeHolder="name" name="Name" />
         <Input width="35vw" placeHolder="@username" name="username" />
