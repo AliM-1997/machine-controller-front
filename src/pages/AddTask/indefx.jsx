5db06e55502e74f84013f53cdaf5d5351be2b53a
AliM-1997 <aliimortada797@gmail.com>
2024-08-27 11:46:18 +0300

    initializing the add task page divisions and title

--- src/pages/AddTask/indefx.jsx
+++ src/pages/AddTask/indefx.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "./style.css";
+import Label from "../../base/Label";
 const AddTask = () => {
-  return <div>hello from add tasks</div>;
+  return (
+    <div className="flex column gap addTask-container">
+      <div>
+        <h2>
+          <Label placeholder="Add/Edit Task" fontWeight="bold" />
+        </h2>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default AddTask;
