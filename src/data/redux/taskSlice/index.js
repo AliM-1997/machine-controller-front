815245534101864a4da4bfbf422791da7e30648e
AliM-1997 <aliimortada797@gmail.com>
2024-08-27 21:54:33 +0300

    setting the task slice initial state and some reduces keys

--- src/data/redux/taskSlice/index.js
+++ src/data/redux/taskSlice/index.js
@@ -1,5 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-const taskSlice = createSlice({ initialState, name: "", reducers });
+const initialState = {
+  id: "",
+  user_id: "",
+  machine_id: "",
+  spare_part_id: "",
+  jobDescription: "",
+  assignedDate: "",
+  dueDate: "",
+  status: "",
+  location: "",
+};
+const taskSlice = createSlice({
+  initialState,
+  name: "take",
+  reducers: { loadTask, updateTask, clearTask },
+});
 export const {} = taskSlice.actions;
 export const taskReducer = taskSlice.reducer;
