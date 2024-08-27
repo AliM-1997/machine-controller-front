3dd96a515e26bb9c39f5e0b26cf89a4a2b9c864c
AliM-1997 <aliimortada797@gmail.com>
2024-08-27 11:56:10 +0300

    adding the machine/user inputs to task

--- src/pages/AddTask/indefx.jsx
+++ src/pages/AddTask/indefx.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./style.css";
 import Label from "../../base/Label";
 import Icon from "../../base/Icon";
-import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleDown,
+  faAngleLeft,
+  faGear,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import Input from "../../base/Input";
 const AddTask = () => {
   return (
     <div className="flex column gap addTask-container">
@@ -12,7 +18,20 @@ const AddTask = () => {
         </h2>
         <Icon icon={faAngleLeft} />
       </div>
-      <div></div>
+      <div className="flex center gap full-width">
+        <Input
+          placeHolder="choose machine "
+          leftIcon={faGear}
+          rightIcon={faAngleDown}
+          width="37vw"
+        />
+        <Input
+          placeHolder="choose user"
+          leftIcon={faUser}
+          rightIcon={faAngleDown}
+          width="37vw"
+        />
+      </div>
       <div></div>
       <div></div>
       <div></div>
