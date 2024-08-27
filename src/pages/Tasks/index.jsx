ce82c471177f2168511fc23d7131f52ac756be10
AliM-1997 <aliimortada797@gmail.com>
2024-08-27 14:11:19 +0300

    adding the all tast filter button

--- src/pages/Tasks/index.jsx
+++ src/pages/Tasks/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./style.css";
 import Label from "../../base/Label";
+import Button from "../../base/Button";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 const Tasks = () => {
   return (
     <div className="flex column  gap task-container">
@@ -12,7 +14,41 @@ const Tasks = () => {
       <div></div>
       <div>
         <div>
-          <div></div>
+          <div className="flex space-btw white-bg center alltask">
+            <div>
+              <Label
+                placeholder="status"
+                fontWeight="bold"
+                backgroundColor="white"
+              />
+            </div>
+            <div className="flex center gap">
+              <Button
+                placeHolder="Date"
+                backgroundColor="primary"
+                width="10vw"
+                textColor="white"
+                leftIcon={faAngleDown}
+                iconColor="white"
+              />
+              <Button
+                placeHolder="Ticket"
+                backgroundColor="primary"
+                width="10vw"
+                textColor="white"
+                leftIcon={faAngleDown}
+                iconColor="white"
+              />
+              <Button
+                placeHolder="Status"
+                backgroundColor="primary"
+                width="10vw"
+                textColor="white"
+                leftIcon={faAngleDown}
+                iconColor="white"
+              />
+            </div>
+          </div>
           <div></div>
         </div>
         <div></div>
