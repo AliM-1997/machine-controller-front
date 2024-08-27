d01748c45a4ee6820255e8bfebba15fd6671ee6e
AliM-1997 <aliimortada797@gmail.com>
2024-08-27 21:50:35 +0300

    initializing the basic structure for the task slice

---
+++ src/data/redux/taskSlice/index.js
@@ -0,0 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const taskSlice = createSlice({ initialState, name: "", reducers });
+export const {} = taskSlice.actions;
+export const taskReducer = taskSlice.reducer;
