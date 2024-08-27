751dd9a557443d3fa46fc7e4944410f7f9282b4d
AliM-1997 <aliimortada797@gmail.com>
2024-08-27 12:46:11 +0300

    adding the props to react date and change its name

--- src/base/Date/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-("./style.css");
-const Date = () => {
-  return <div></div>;
-};
-
-export default Date;
---
+++ src/base/ReactDate/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+("./style.css");
+const Date = ({
+  placeHolder = "Select a date",
+  leftIcon = null,
+  rightIcon = null,
+  backgroundColor = "white",
+  textColor = "black",
+  width = "",
+  onChange,
+  name = "",
+  border = true,
+}) => {
+  return <div></div>;
+};
+
+export default Date;
