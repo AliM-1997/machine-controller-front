b47f38e363bdbdcdc335908358c09824c06d0e2d
AliM-1997 <aliimortada797@gmail.com>
2024-08-27 22:14:30 +0300

    setting task reducer to store

--- src/data/redux/store.js
+++ src/data/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./userSlice";
+import { taskReducer } from "./taskSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    task: taskReducer,
   },
 });
 export default store;
