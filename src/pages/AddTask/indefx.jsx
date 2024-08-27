f62ee91882ccc36986b2ddb17e31bfa051530df1
AliM-1997 <aliimortada797@gmail.com>
2024-08-27 11:50:08 +0300

    adding the back icon to add user page

--- src/pages/AddTask/indefx.jsx
+++ src/pages/AddTask/indefx.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./style.css";
 import Label from "../../base/Label";
+import Icon from "../../base/Icon";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 const AddTask = () => {
   return (
     <div className="flex column gap addTask-container">
-      <div>
+      <div className="flex space-btw">
         <h2>
           <Label placeholder="Add/Edit Task" fontWeight="bold" />
         </h2>
+        <Icon icon={faAngleLeft} />
       </div>
       <div></div>
       <div></div>
