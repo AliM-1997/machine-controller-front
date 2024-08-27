cd6eb6a7837621845145ec8f336b80e82a5336cb
AliM-1997 <aliimortada797@gmail.com>
2024-08-27 16:52:20 +0300

    adding the switch cases to highlight lable bas os the text

--- src/base/HighlightLable/index.jsx
+++ src/base/HighlightLable/index.jsx
@@ -5,6 +5,32 @@ const HighlightLabel = ({ highlight = true, placeHolder = "text" }) => {
   let textColor = "";
   let backgroundColor = "";
 
+  switch (placeHolder) {
+    case "Pending" || " Requiring Attention":
+      textColor = "yellow";
+      backgroundColor = "Lyellow";
+      break;
+    case "In Progress":
+      textColor = "blue";
+      backgroundColor = "Lblue";
+      break;
+    case "Completed" || "Active":
+      textColor = "green";
+      backgroundColor = "Lgreen";
+      break;
+    case "Risked" || "Under Maintenace":
+      textColor = "red";
+      backgroundColor = "Lred";
+      break;
+    case "Delayed":
+      textColor = "orange";
+      backgroundColor = "Lorange";
+      break;
+    default:
+      textColor = "black";
+      backgroundColor = "white";
+  }
+
   return (
     <div className={`highlight-label ${textColor}-txt ${backgroundColor}-bg`}>
       {placeHolder}
