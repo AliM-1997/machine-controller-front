6215c806a70adfaca3d02079c57c58c1956939d9
AliM-1997 <aliimortada797@gmail.com>
2024-08-27 16:51:24 +0300

    adding the props to highlighting lable

--- src/base/HighlightLable/index.jsx
+++ src/base/HighlightLable/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./style.css";
-const HighlightLable = () => {
-  return <div></div>;
+
+const HighlightLabel = ({ highlight = true, placeHolder = "text" }) => {
+  let textColor = "";
+  let backgroundColor = "";
+
+  return (
+    <div className={`highlight-label ${textColor}-txt ${backgroundColor}-bg`}>
+      {placeHolder}
+    </div>
+  );
 };
 
-export default HighlightLable;
+export default HighlightLabel;
