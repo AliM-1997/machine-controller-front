2307b405152458eb2a25d69419dd27c4dc718cb2
AliM-1997 <aliimortada797@gmail.com>
2024-08-28 00:39:29 +0300

    adding the create task to task remote

--- src/data/remote/Tasks.js
+++ src/data/remote/Tasks.js
@@ -1,9 +1,24 @@
 import { requestApi } from "../../utils/request";
+import { RequestMethods } from "../../utils/request_methods";
 
 export const Tasks = {
   GetAllTasks: async () => {
     try {
       const data = await requestApi({ route: "/task" });
+      return data;
+    } catch (error) {
+      console.error("Error creating user:", error.message);
+      throw error;
+    }
+  },
+  CreateTask: async (formData) => {
+    try {
+      const data = await requestApi({
+        route: "/task",
+        requestMethod: RequestMethods.POST,
+        body: formData,
+      });
+      console.log("data from task remote", data);
       return data;
     } catch (error) {
       console.error("Error creating user:", error.message);
