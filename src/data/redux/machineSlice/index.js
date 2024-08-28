8196b3520300680037677257b5b2719aa59892f3
AliM-1997 <aliimortada797@gmail.com>
2024-08-28 22:13:10 +0300

    initializing the initail state and the base key reducer for machine slice

--- src/data/redux/machineSlice/index.js
+++ src/data/redux/machineSlice/index.js
@@ -1,9 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+initialState = {
+  id: "",
+  name: "",
+  serial_number: "",
+  status: "active",
+  location: "",
+  image_path: "",
+  description: "",
+  last_maintenance: "",
+  unit_per_hour: "",
+  last_maintenance: "",
+};
 const machineSlice = createSlice({
   initialState,
   name: "machine",
-  reducers,
+  reducers: {
+    LoadMachine,
+    updateMachine,
+    clearMachine,
+  },
 });
 export const {} = machineSlice.actions;
 export const machineReducr = machineSlice.reducer;
