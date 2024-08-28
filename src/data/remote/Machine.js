0755b9a828edcf41e4b55c869b486f051220178b
AliM-1997 <aliimortada797@gmail.com>
2024-08-28 17:23:01 +0300

    defining the create machine to machine remote file

--- src/data/remote/Machine.js
+++ src/data/remote/Machine.js
@@ -1,7 +1,23 @@
+import { requestApi } from "../../utils/request";
+import { RequestMethods } from "../../utils/request_methods";
+
 export const Machines = {
-  CreateMachine,
-  UpadateMachine,
-  DeleteMachine,
-  GetMachineById,
-  GetAllMachines,
+  CreateMachine: async (formData) => {
+    try {
+      const data = await requestApi({
+        route: "/machine",
+        requestMethod: RequestMethods.POST,
+        body: formData,
+      });
+      console.log("create from remote", data);
+      return data;
+    } catch (error) {
+      console.error("Error creating user:", error.message);
+      throw error;
+    }
+  },
+  //   UpadateMachine,
+  //   DeleteMachine,
+  //   GetMachineById,
+  //   GetAllMachines,
 };
