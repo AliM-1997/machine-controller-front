1148c025109454d9cc484ee10ab1e04a5d99eee2
AliM-1997 <aliimortada797@gmail.com>
2024-08-28 19:24:20 +0300

    adding the page containers and the title to all machine page

--- src/pages/AllMachines/index.jsx
+++ src/pages/AllMachines/index.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "./style.css";
 import Header from "../../components/Header";
+import Label from "../../base/Label";
 const AllMachines = () => {
   const options = [
     { label: "All Machines", url: "allmachines" },
     { label: "Add Machine", url: "addmachine" },
   ];
   return (
-    <div>
+    <div className="flex column gap">
       <Header pageName="All Machines" options={options} />
+      <div className="flex column gap machines-container">
+        <div>
+          <h2>
+            <Label placeholder="All Machines" />
+          </h2>
+        </div>
+      </div>
     </div>
   );
 };
