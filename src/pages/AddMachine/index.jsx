44613060ecea452d3ac5c51cd3c1653c8df75eb9
AliM-1997 <aliimortada797@gmail.com>
2024-08-28 16:10:20 +0300

    create Add machine component

---
+++ src/pages/AddMachine/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style.css";
+const AddMachine = () => {
+  return <div>hello from AddMachine</div>;
+};
+
+export default AddMachine;
--- src/pages/Machines/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./style.css";
-const Machines = () => {
-  return <div>hello from machines</div>;
-};
-
-export default Machines;
