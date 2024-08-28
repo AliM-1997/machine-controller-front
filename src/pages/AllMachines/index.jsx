3e54ff4838ed4d78d01d881572f0fc0bac599394
AliM-1997 <aliimortada797@gmail.com>
2024-08-28 19:45:44 +0300

    adding the search /addbars  to all machine page

--- src/pages/AllMachines/index.jsx
+++ src/pages/AllMachines/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./style.css";
 import Header from "../../components/Header";
 import Label from "../../base/Label";
+import Input from "../../base/Input";
+import Button from "../../base/Button";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 const AllMachines = () => {
   const options = [
     { label: "All Machines", url: "allmachines" },
@@ -15,6 +18,19 @@ const AllMachines = () => {
           <h2>
             <Label placeholder="All Machines" />
           </h2>
+        </div>
+        <div className=" flex row  full-width space-btw search-bar">
+          <Input
+            placeHolder="search machine"
+            leftIcon={faSearch}
+            width="20vw"
+            required={false}
+          />
+          <Button
+            width="8vw"
+            placeHolder="add machine"
+            backgroundColor="primary"
+          />
         </div>
       </div>
     </div>
