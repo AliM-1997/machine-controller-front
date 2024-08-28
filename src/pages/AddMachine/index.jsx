c59a90dc7cb38e63ba737c5ce71df3efca58e7b4
AliM-1997 <aliimortada797@gmail.com>
2024-08-28 16:31:26 +0300

    adding the image container to addmachine page

--- src/pages/AddMachine/index.jsx
+++ src/pages/AddMachine/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./style.css";
 import Header from "../../components/Header";
+import Label from "../../base/Label";
+import Button from "../../base/Button";
+import DisplayImage from "../../base/DisplayImage";
 const AddMachine = () => {
   const options = [
     {
@@ -10,7 +13,31 @@ const AddMachine = () => {
   ];
   return (
     <div>
-      <Header pageName="Add/Edit Machine" options={options} />
+      <Header pageName="Machines" options={options} />
+      <div className="flex column full-width addMachine-container gap">
+        <h2>
+          <Label placeholder="Add/Edit Machine" />
+        </h2>
+        <div className="flex space-btw image-container">
+          <div>
+            <DisplayImage width="150px" height="150px" borderRadius="12px" />
+          </div>
+          <div className="flex gap">
+            <Button
+              placeHolder="Upload Image"
+              width="10vw"
+              backgroundColor="primary"
+              textColor="white"
+            />
+            <Button
+              placeHolder="Delete"
+              width="8vw"
+              backgroundColor="primary"
+              textColor=" white"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
