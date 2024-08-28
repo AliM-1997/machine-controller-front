261e493e8211bd17146bb93227edc2f19d9b726f
AliM-1997 <aliimortada797@gmail.com>
2024-08-29 01:07:07 +0300

    making the intups dynamic if thier is machine to edit

--- src/pages/AddMachine/index.jsx
+++ src/pages/AddMachine/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import Header from "../../components/Header";
 import Label from "../../base/Label";
@@ -17,7 +17,10 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { Machines } from "../../data/remote/Machine";
 import { Functions } from "../../utils/reusableFunctions";
+import { useSelector } from "react-redux";
 const AddMachine = () => {
+  const machine = useSelector((global) => global.machine);
+  console.log("from slice", machine);
   const [formData, setFormData] = useState({
     name: "",
     serial_number: "",
@@ -29,18 +32,27 @@ const AddMachine = () => {
     unit_per_hour: "",
     last_maintenance: "",
   });
-  console.log(formData);
-  const handleFormData = (key, value) => {
+
+  useEffect(() => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      ...machine,
+    }));
+  }, [machine]);
+
+  const ChangingFormData = (key, value) => {
     setFormData({
       ...formData,
       [key]: value,
     });
   };
+
   const handleCreateMachine = async () => {
     const dataToSend = {
       ...formData,
       last_maintenance: Functions.ToDateformat(formData.last_maintenance),
     };
+
     try {
       const data = await Machines.CreateMachine(dataToSend);
       console.log(data);
@@ -56,122 +68,131 @@ const AddMachine = () => {
     <div>
       <Header pageName="Machines" options={options} className="container" />
       <div className="flex column full-width addMachine-container gap">
-        <h2>
-          <Label placeholder="Add/Edit Machine" />
-        </h2>
-        <div className="flex space-btw image-container">
-          <div>
-            <DisplayImage width="150px" height="150px" borderRadius="12px" />
+        <div className="flex column gap">
+          <h2>
+            <Label placeholder="Add/Edit Machine" />
+          </h2>
+          <div className="flex space-btw image-container">
+            <div>
+              <DisplayImage width="150px" height="150px" borderRadius="12px" />
+            </div>
+            <div className="flex gap">
+              <Button
+                placeHolder="Upload Image"
+                width="10vw"
+                backgroundColor="primary"
+                textColor="white"
+              />
+              <Button
+                placeHolder="Delete"
+                width="8vw"
+                backgroundColor="primary"
+                textColor=" white"
+              />
+            </div>
           </div>
-          <div className="flex gap">
+        </div>
+
+        <div className="flex column gap full-height machine-inputs">
+          <div className="flex space-btw full-width">
+            <Input
+              placeHolder={machine.name || "name"}
+              name="Name"
+              width="37vw"
+              leftIcon={faUser}
+              type="text"
+              onChange={(e) => {
+                ChangingFormData("name", e.target.value);
+              }}
+            />
+            <Input
+              leftIcon={faAt}
+              placeHolder={machine.serial_number || "serial number"}
+              name="Serial Number"
+              width="37vw"
+              type="text"
+              onChange={(e) => {
+                ChangingFormData("serial_number", e.target.value);
+              }}
+            />
+          </div>
+          <div className="flex space-btw full-width">
+            <Input
+              name="Units Per Hour"
+              width="37vw"
+              leftIcon={faHashtag}
+              type="text"
+              onChange={(e) => {
+                ChangingFormData("unit_per_hour", e.target.value);
+              }}
+            />
+            <Input
+              textColor="white"
+              leftIcon={faAngleDown}
+              placeHolder={machine.status || "pending"}
+              iconColor="black"
+              width="37vw"
+              name="Status"
+              type="text"
+              onChange={(e) => {
+                ChangingFormData("status", e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="full-width">
+            <Input
+              placeHolder={machine.location || "location"}
+              name="Location"
+              leftIcon={faLocation}
+              type="text"
+              onChange={(e) => {
+                ChangingFormData("location", e.target.value);
+              }}
+            />
+          </div>
+          <div className="full-width">
+            <Input
+              placeHolder={machine.description || "description"}
+              name="machine Description"
+              leftIcon={faClipboard}
+              type="text"
+              onChange={(e) => {
+                ChangingFormData("description", e.target.value);
+              }}
+            />
+          </div>
+          <div className="full-width">
+            <ReactDate
+              leftIcon={faCalendarDays}
+              name="Last Maintenance"
+              placeHolder={
+                formData.last_maintenance
+                  ? new Date(formData.last_maintenance)
+                      .toISOString()
+                      .slice(0, 10)
+                  : "dd/MM/yyyy"
+              }
+              onChange={(e) => ChangingFormData("last_maintenance", e)}
+            />
+          </div>
+
+          <div className="flex end gap">
             <Button
-              placeHolder="Upload Image"
+              placeHolder="Delete"
               width="10vw"
               backgroundColor="primary"
               textColor="white"
             />
             <Button
-              placeHolder="Delete"
-              width="8vw"
+              placeHolder="Save"
               backgroundColor="primary"
-              textColor=" white"
+              width="10vw"
+              textColor="white"
+              onClick={handleCreateMachine}
             />
           </div>
         </div>
-        <div className="flex space-btw full-width">
-          <Input
-            placeHolder="name"
-            name="Name"
-            width="37vw"
-            leftIcon={faUser}
-            type="text"
-            onChange={(e) => {
-              handleFormData("name", e.target.value);
-            }}
-          />
-          <Input
-            leftIcon={faAt}
-            placeHolder="serial number"
-            name="Serial Number"
-            width="37vw"
-            type="text"
-            onChange={(e) => {
-              handleFormData("serial_number", e.target.value);
-            }}
-          />
-        </div>
-        <div className="flex space-btw full-width">
-          <Input
-            name="Units Per Hour"
-            width="37vw"
-            leftIcon={faHashtag}
-            type="text"
-            onChange={(e) => {
-              handleFormData("unit_per_hour", e.target.value);
-            }}
-          />
-          <Input
-            textColor="white"
-            leftIcon={faAngleDown}
-            iconColor="black"
-            width="37vw"
-            name="Status"
-            type="text"
-            onChange={(e) => {
-              handleFormData("status", e.target.value);
-            }}
-          />
-        </div>
-
-        <div className="full-width">
-          <Input
-            placeHolder={"location"}
-            name="Location"
-            leftIcon={faLocation}
-            type="text"
-            onChange={(e) => {
-              handleFormData("location", e.target.value);
-            }}
-          />
-        </div>
-        <div className="full-width">
-          <Input
-            placeHolder={"task"}
-            name="machine Description"
-            leftIcon={faClipboard}
-            type="text"
-            onChange={(e) => {
-              handleFormData("description", e.target.value);
-            }}
-          />
-        </div>
-        <div className="full-width">
-          <ReactDate
-            leftIcon={faCalendarDays}
-            name="Last Maintenace"
-            placeHolder={
-              formData.assignedDate
-                ? formData.assignedDate.toISOString().slice(0, 10)
-                : "dd/MM/yyyy"
-            }
-            onChange={(e) => handleFormData("last_maintenance", e)}
-          />
-        </div>
-      </div>
-      <div className="flex end gap">
-        <Button
-          placeHolder="Delete"
-          width="10vw"
-          backgroundColor="primary"
-          textColor="white"
-        />
-        <Button
-          placeHolder="Save"
-          backgroundColor="primary"
-          width="10vw"
-          textColor="white"
-          onClick={handleCreateMachine}
-        />
       </div>
     </div>
   );
