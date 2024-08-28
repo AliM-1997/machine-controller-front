a77126e1632f177fc9d8076300a43474f539e2db
AliM-1997 <aliimortada797@gmail.com>
2024-08-28 14:03:26 +0300

    deleting the header from layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -6,32 +6,8 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   const [clickedPage, setClickedPage] = useState(null);
-  const [showChooseInput, setShowChooseInput] = useState(true);
   const handlePageChange = (newPageName) => {
     setClickedPage(newPageName);
-
-    switch (newPageName) {
-      case "Dashboard":
-        setShowChooseInput(false);
-        break;
-      case "Machines":
-        setShowChooseInput(true);
-        break;
-      case "Tasks":
-        setShowChooseInput(true);
-        break;
-      case "Predictions":
-        setShowChooseInput(true);
-        break;
-      case "Alerts":
-        setShowChooseInput(false);
-        break;
-      case "User Management":
-        setShowChooseInput(true);
-        break;
-      default:
-        setShowChooseInput(true);
-    }
   };
 
   return (
@@ -40,7 +16,6 @@ const Layout = () => {
         <NavBar onNavigate={handlePageChange} isClick={clickedPage} />
       </div>
       <div className="flex column container-2">
-        <Header pageName={clickedPage} showChooseInput={showChooseInput} />
         <Outlet />
       </div>
     </div>
