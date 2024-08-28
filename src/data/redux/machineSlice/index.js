f918e549dfcf83026b7116974afe4faf9e626be0
AliM-1997 <aliimortada797@gmail.com>
2024-08-28 22:10:27 +0300

    adding the base structure for machine slice

---
+++ src/data/redux/machineSlice/index.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const machineSlice = createSlice({
+  initialState,
+  name: "machine",
+  reducers,
+});
+export const {} = machineSlice.actions;
+export const machineReducr = machineSlice.reducer;
