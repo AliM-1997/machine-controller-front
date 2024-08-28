88e95970a9e8e8f46746a19109b0cd7081f0695b
AliM-1997 <aliimortada797@gmail.com>
2024-08-28 19:55:31 +0300

    adding the outer shape of the machine card and initialize the image

--- src/components/MachineCard/index.jsx
+++ src/components/MachineCard/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "./style.css";
-const MachineCard = () => {
-  return <div></div>;
+import DisplayImage from "../../base/DisplayImage";
+const MachineCard = ({ machineData }) => {
+  return (
+    <div className="machine-card flex row white-bg" width="70vw" height="20vh">
+      <div>
+        <DisplayImage
+          url={machineData.image_path}
+          width="23vw"
+          height="25vh"
+          borderRadius="24px 0 0 24px"
+        />
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default MachineCard;
