518a9e2f457f584650b7306ca2160a94f55af1b0
AliM-1997 <aliimortada797@gmail.com>
2024-08-30 00:03:23 +0300

    adding eff- mtt statistics to dash board

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import BarGraph from "../../components/BarGraph"; // Reusable BarGraph component
 import { MachineStatistics } from "../../data/remote/machineStatistics";
 import Header from "../../components/Header";
+import Label from "../../base/Label";
 
 const Dashboard = () => {
   const [statistics, setStatistics] = useState([]);
@@ -18,8 +19,10 @@ const Dashboard = () => {
   return (
     <div>
       <Header />
-      <div className="flex center column gap">
-        <h2>Machine Statistics Dashboard</h2>
+      <div className="flex center column gap dachboard-container">
+        <div className="">
+          <h2>Machine Statistics</h2>
+        </div>
         <div>
           <div className="flex space-around gap">
             <div className="">
@@ -34,6 +37,24 @@ const Dashboard = () => {
                 data={statistics}
                 type="operationalTime"
                 title="Operation Time"
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="flex space-around gap">
+            <div className="">
+              <BarGraph
+                data={statistics}
+                type="MTBF_MTTR_MTTD"
+                title="MTBF, MTTR, MTTD"
+              />
+            </div>
+            <div className="">
+              <BarGraph
+                data={statistics}
+                type="efficiency_availability"
+                title="Efficiency and Availability"
               />
             </div>
           </div>
