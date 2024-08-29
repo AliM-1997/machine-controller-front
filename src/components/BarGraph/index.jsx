2a5a50fb6ea1016bb953955cdc4ad8b1049b8984
AliM-1997 <aliimortada797@gmail.com>
2024-08-29 22:14:52 +0300

    initializing the static basic char to compoennt

--- src/components/BarGraph/index.jsx
+++ src/components/BarGraph/index.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Bar } from "react-chartjs-2";
 import "./style.css";
 import {
   Chart as ChartJS,
@@ -17,8 +18,45 @@ ChartJS.register(
   Tooltip,
   Legend
 );
+
 const BarGraph = () => {
-  return <div></div>;
+  const [chartData, setChartData] = useState(null);
+
+  useEffect(() => {
+    // Simulate fetching data or processing it
+    const data = {
+      labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      datasets: [
+        {
+          label: "MTTR",
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: "rgba(255, 99, 132, 0.5)",
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+        },
+      ],
+    };
+    setChartData(data);
+  }, []);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Machine Statistics",
+      },
+    },
+  };
+
+  return (
+    <div className="stat-container ">
+      <Bar data={chartData} options={options} />;
+    </div>
+  );
 };
 
 export default BarGraph;
