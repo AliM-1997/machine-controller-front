f40d129a93fd461653e3a0c57ac9917b988b4150
AliM-1997 <aliimortada797@gmail.com>
2024-08-29 16:56:53 +0300

    adding react hooks to save the fetched data

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import { MachineStatistics } from "../../data/remote/machineStatistics";
 const Dashboard = () => {
+  const [statistics, setStatistics] = useState([]);
+  console.log("statistics", statistics);
   const handleAllStatistics = async () => {
     const data = await MachineStatistics.GetALLStatistics();
+    setStatistics(data.statistic);
     console.log("statistic from dashboard", data);
   };
-  handleAllStatistics();
+
+  useEffect(() => {
+    handleAllStatistics();
+  }, []);
   return <div>hello from Dashboard</div>;
 };
 
