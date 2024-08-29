32aed00699e2c65a9b2223e9166c08198701bd8a
AliM-1997 <aliimortada797@gmail.com>
2024-08-29 21:46:08 +0300

    importing chart libraries and register it to graph chart component

--- src/components/BarGraph/index.jsx
+++ src/components/BarGraph/index.jsx
@@ -1,4 +1,22 @@
 import React from "react";
+import "./style.css";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 const BarGraph = () => {
   return <div></div>;
 };
