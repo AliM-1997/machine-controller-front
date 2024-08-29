6ad3378acdb3130f52b2ec9fc6069478f50f460e
AliM-1997 <aliimortada797@gmail.com>
2024-08-29 16:48:45 +0300

    handling fetching all machine data in dachboard bage

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./style.css";
+import { MachineStatistics } from "../../data/remote/machineStatistics";
 const Dashboard = () => {
+  const handleAllStatistics = async () => {
+    const data = await MachineStatistics.GetALLStatistics();
+    console.log("statistic from dashboard", data);
+  };
+  handleAllStatistics();
   return <div>hello from Dashboard</div>;
 };
 
