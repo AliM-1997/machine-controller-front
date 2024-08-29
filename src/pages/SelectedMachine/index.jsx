5ae0790880c66bfcc5f935c9f3146e296b902c03
AliM-1997 <aliimortada797@gmail.com>
2024-08-29 04:34:46 +0300

    reading param and  start implementing the page lablel/image of selected machine

--- src/pages/SelectedMachine/index.jsx
+++ src/pages/SelectedMachine/index.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import Header from "../../components/Header";
+import Label from "../../base/Label";
+import DisplayImage from "../../base/DisplayImage";
+import { useParams } from "react-router-dom";
 
 const SelectedMachine = () => {
+  const { id } = useParams();
   const options = [
     { label: "Selected Machine", url: "selectedmachine" },
     { label: "Add Machine", url: "addmachine" },
     { label: "All Machines", url: "allmachines" },
   ];
+
   return (
     <div>
       <Header pageName="Selected Machine" options={options} />
+      <div>
+        <div>
+          <Label placeholder="Selected Machine" />
+        </div>
+        <div>
+          <DisplayImage />
+        </div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
