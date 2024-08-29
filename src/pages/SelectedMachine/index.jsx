a261d55c3982202761b7dba3c641fc3b64bfb14d
AliM-1997 <aliimortada797@gmail.com>
2024-08-29 04:38:02 +0300

    handle get machine by id to selected machine

--- src/pages/SelectedMachine/index.jsx
+++ src/pages/SelectedMachine/index.jsx
@@ -4,21 +4,37 @@ import Header from "../../components/Header";
 import Label from "../../base/Label";
 import DisplayImage from "../../base/DisplayImage";
 import { useParams } from "react-router-dom";
+import { Machines } from "../../data/remote/Machine";
 
 const SelectedMachine = () => {
   const { id } = useParams();
+
   const options = [
     { label: "Selected Machine", url: "selectedmachine" },
     { label: "Add Machine", url: "addmachine" },
     { label: "All Machines", url: "allmachines" },
   ];
-
+  const handleMachineById = async () => {
+    try {
+      const data = await Machines.GetMachineById(id);
+      console.log("asdfsadfdsdfds", data);
+    } catch (error) {
+      console.error("Error fetching machine data:", error);
+    }
+  };
+  useEffect(() => {
+    {
+      handleMachineById();
+    }
+  }, []);
   return (
     <div>
       <Header pageName="Selected Machine" options={options} />
-      <div>
+      <div className=" flex column selectedMachine-container">
         <div>
-          <Label placeholder="Selected Machine" />
+          <h2>
+            <Label placeholder="Selected Machine" />
+          </h2>
         </div>
         <div>
           <DisplayImage />
