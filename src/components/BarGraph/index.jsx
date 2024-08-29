7a846ea59f5e6df67474ddec906a68b4f31f0850
AliM-1997 <aliimortada797@gmail.com>
2024-08-29 22:39:19 +0300

    getting the first graph lables and color to switch

--- src/components/BarGraph/index.jsx
+++ src/components/BarGraph/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Bar } from "react-chartjs-2";
 import "./style.css";
 import {
@@ -10,6 +10,7 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -22,19 +23,30 @@ ChartJS.register(
 const BarGraph = ({ data, type }) => {
   const labels = data.map((item) => item.date);
   let datasets = [];
+
   switch (type) {
-    case "uptime":
-      datasets.push({
-        label: "Uptime Hours",
-        data: data.map((item) => parseFloat(item.uptimeHours)),
-        backgroundColor: "rgba(54, 162, 235, 0.2)",
-        borderColor: "rgba(54, 162, 235, 1)",
-        borderWidth: 1,
-      });
+    case "uptime_downtime":
+      datasets.push(
+        {
+          label: "Uptime",
+          data: data.map((item) => parseFloat(item.uptimeHours)),
+          backgroundColor: "rgba(0, 255, 0, 1)",
+          borderWidth: 1,
+          borderRadius: 6,
+        },
+        {
+          label: "Downtime",
+          data: data.map((item) => parseFloat(item.downtime)),
+          backgroundColor: "rgba(255, 0, 0, 1)",
+          borderWidth: 1,
+          borderRadius: 6,
+        }
+      );
       break;
     default:
       break;
   }
+
   const chartData = {
     labels: labels,
     datasets: datasets,
@@ -54,8 +66,8 @@ const BarGraph = ({ data, type }) => {
   };
 
   return (
-    <div className="stat-container ">
-      <Bar data={chartData} options={options} />;
+    <div className="stat-container">
+      <Bar data={chartData} options={options} />
     </div>
   );
 };
