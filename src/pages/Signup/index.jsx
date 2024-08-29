8d98a8c0fbfd8e9e00043e1da1a0461e38f1311d
AliM-1997 <aliimortada797@gmail.com>
2024-08-29 16:08:51 +0300

    introducing header to login and signup

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -4,6 +4,7 @@ import Input from "../../base/Input";
 import Button from "../../base/Button";
 import { useNavigate } from "react-router-dom";
 import { authRemote } from "../../data/remote/Auth_user";
+import Header from "../../components/Header";
 const Signup = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -37,71 +38,76 @@ const Signup = () => {
     console.log(data);
     navigate("/login");
   };
+
   return (
-    <div className="flex column center page gap">
-      <div className="flex column center white-bg gap signup-container">
-        <div className="flex column center gap">
-          <h1>
-            <span className="highlight">D</span>ustry
-          </h1>
-          <div className="flex  black-bg switch">
-            <Button
-              placeHolder="Login"
-              width="125px"
-              backgroundColor="black"
-              textColor="white"
-              onClick={handleswitch}
+    <div>
+      <Header />
+
+      <div className="flex column center gap page-oo">
+        <div className="flex column center white-bg gap signup-container gape-oo">
+          <div className="flex column center gap">
+            <h1>
+              <span className="highlight">D</span>ustry
+            </h1>
+            <div className="flex  black-bg switch">
+              <Button
+                placeHolder="Login"
+                width="125px"
+                backgroundColor="black"
+                textColor="white"
+                onClick={handleswitch}
+              />
+              <Button
+                placeHolder="Signup"
+                backgroundColor="primary"
+                width="125px"
+                textColor="white"
+              />
+            </div>
+          </div>
+          <div className="flex column signup-input ">
+            <Input
+              placeHolder="name"
+              width="400px"
+              name="Name"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
-            <Button
-              placeHolder="Signup"
-              backgroundColor="primary"
-              width="125px"
-              textColor="white"
+            <Input
+              placeHolder="example@gamil.com"
+              width="400px"
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeHolder="password"
+              width="400px"
+              name="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Input
+              placeHolder="password"
+              type="password"
+              width="400px"
+              name="Confirmed Password"
+              value={confirmedPassword}
+              onChange={(e) => setConfirmedPassword(e.target.value)}
             />
           </div>
-        </div>
-        <div className="flex column signup-input ">
-          <Input
-            placeHolder="name"
-            width="400px"
-            name="Name"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <Input
-            placeHolder="example@gamil.com"
-            width="400px"
-            name="email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            placeHolder="password"
-            width="400px"
-            name="Password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Input
-            placeHolder="password"
-            type="password"
-            width="400px"
-            name="Confirmed Password"
-            value={confirmedPassword}
-            onChange={(e) => setConfirmedPassword(e.target.value)}
-          />
-        </div>
-        <div className="flex column gap">
-          <Button
-            backgroundColor="primary"
-            width="400px"
-            textColor="white"
-            placeHolder="Signup"
-            onClick={handelSignup}
-          />
+          <div className="flex column gap">
+            <Button
+              backgroundColor="primary"
+              width="400px"
+              textColor="white"
+              placeHolder="Signup"
+              onClick={handelSignup}
+            />
+          </div>
         </div>
       </div>
     </div>
