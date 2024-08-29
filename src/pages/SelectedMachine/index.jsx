c6977e6a0b87f3d135953cff018f2f9e9185cc1c
AliM-1997 <aliimortada797@gmail.com>
2024-08-29 03:26:52 +0300

    adding the header to selected machine

--- src/pages/SelectedMachine/index.jsx
+++ src/pages/SelectedMachine/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./style.css";
+import Header from "../../components/Header";
+
 const SelectedMachine = () => {
-  return <div></div>;
+  const options = [
+    { label: "Selected Machine", url: "selectedmachine" },
+    { label: "Add Machine", url: "addmachine" },
+    { label: "All Machines", url: "allmachines" },
+  ];
+  return (
+    <div>
+      <Header pageName="Selected Machine" options={options} />
+    </div>
+  );
 };
 
 export default SelectedMachine;
