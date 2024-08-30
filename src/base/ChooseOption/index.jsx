3eeec9dc5ec36e81c98d8af6073faaae5ca2d840
AliM-1997 <aliimortada797@gmail.com>
2024-08-31 01:09:05 +0300

    initializing the choose input component props

--- src/base/ChooseOption/index.jsx
+++ src/base/ChooseOption/index.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import "./style.css";
-const ChooseOption = () => {
+const ChooseOption = ({
+  options = [],
+  onSelect,
+  placeholder = "Select an option",
+  backgroundColor = "white",
+  width,
+  textColor = "black",
+  leftIcon,
+  rightIcon,
+  iconColor,
+  name,
+  required = true,
+  s,
+}) => {
   return <div></div>;
 };
 
