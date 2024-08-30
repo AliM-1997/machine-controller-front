d1aa7090368b068643b3ddb4b7455b7e01f93c17
AliM-1997 <aliimortada797@gmail.com>
2024-08-30 17:10:16 +0300

    imoprting the statistics slice for the store

--- src/data/redux/store.js
+++ src/data/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./userSlice";
 import { taskReducer } from "./taskSlice";
 import { machineReducre } from "./machineSlice";
+import { statisticReducer } from "./statisticSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     task: taskReducer,
     machine: machineReducre,
+    statistic: statisticReducer,
   },
 });
 export default store;
