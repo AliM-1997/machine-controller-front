2acb445ff4d4a8974258a7776b4218772aa9aecd
AliM-1997 <aliimortada797@gmail.com>
2024-08-31 15:33:33 +0300

    creating dta slice to store the rendered data

---
+++ src/data/redux/renderSlice/index.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const data = createSlice({
+  initialState,
+  name: "data",
+  reducers,
+});
+
+export const {} = data.actions;
+export const dataReducer = data.reducer;
