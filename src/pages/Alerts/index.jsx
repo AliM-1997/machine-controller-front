353c1d5e119e363e6af55ca617d27cbf0e8d708b
AliM-1997 <aliimortada797@gmail.com>
2024-09-02 22:49:43 +0300

    handeling the unread notification in alert page

--- src/pages/Alerts/index.jsx
+++ src/pages/Alerts/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./style.css";
 import Header from "../../components/Header";
+import { Notifications } from "../../data/remote/notification";
 const Alerts = () => {
+  const handleUnReadNotification = async () => {
+    const response = await Notifications.unread();
+    console.log("unread notifications in alerts", response);
+  };
+  handleUnReadNotification();
+
   return (
     <div>
       <Header pageName={"Alerts"} showChooseInput={false} />
+      <div className="flex column"></div>
     </div>
   );
 };
