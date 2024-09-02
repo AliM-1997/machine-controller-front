bc6ae3486511b545e2494c0614081b5a511dcc7f
AliM-1997 <aliimortada797@gmail.com>
2024-09-03 01:57:26 +0300

    adding the  needed hooks to handle readed and unreaded notification

--- src/pages/Alerts/index.jsx
+++ src/pages/Alerts/index.jsx
@@ -1,38 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import Header from "../../components/Header";
 import { Notifications } from "../../data/remote/notification";
+import Button from "../../base/Button";
 const Alerts = () => {
+  const [nonRead, setNoneRead] = useState([]);
   const handleUnReadNotification = async () => {
     const response = await Notifications.UnReadNotification();
     console.log("unread notifications in alerts", response);
+    setNoneRead(response.notifications || []);
   };
-  handleUnReadNotification();
-  const handleMarkNotificationAsRead = async () => {
-    const notificationId = "e3fabc38-a818-4479-a12c-8e83a9cbaea5";
+
+  const handleMarkNotificationAsRead = async (notificationId) => {
     const response = await Notifications.MarkNottificationAsRead(
       notificationId
     );
-    console.log("mark as read notification", response);
+    setNoneRead(
+      nonRead.filter((notification) => notification.id !== notificationId)
+    );
   };
-  handleMarkNotificationAsRead();
+
+  useEffect(() => {
+    handleUnReadNotification();
+  }, []);
+
   return (
     <div>
       <Header pageName={"Alerts"} showChooseInput={false} />
       <div className="flex column notification-outer">
-        <div className=" flex title-notification">
+        <div className="flex title-notification">
           <h3>Notifications</h3>
         </div>
-        <div className="flex column center gap padding-30px full-width">
-          <div className=" flex full-width Lprimary-bg padding-30px space-btw">
-            <div>
-              <p>notification</p>
-            </div>
-            <div>
-              <p className="blue-txt mark-read">mark as read</p>
-            </div>
-          </div>
-        </div>
+        <div className="flex column center gap padding-30px full-width white-bg"></div>
       </div>
     </div>
   );
