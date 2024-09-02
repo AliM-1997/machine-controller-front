86e450aab414dd7fdbee8d84c5e258ae5b246368
AliM-1997 <aliimortada797@gmail.com>
2024-09-02 22:50:09 +0300

    creating mark notification as read in remote file

--- src/data/remote/notification.js
+++ src/data/remote/notification.js
@@ -1,11 +1,23 @@
 import { requestApi } from "../../utils/request";
 
 export const Notifications = {
-  unread: async () => {
+  UnReadNotification: async () => {
     try {
-      data = await requestApi({});
+      const data = await requestApi({ route: "/notifications/unread" });
+      return data;
     } catch (error) {
-      console.error(message.error);
+      console.error(error);
+      throw error;
+    }
+  },
+  MarkNottificationAsRead: async (notificationId) => {
+    try {
+      const data = await requestApi({
+        route: `/notifications/${notificationId}/read`,
+      });
+      return data;
+    } catch (error) {
+      console.error(error);
       throw error;
     }
   },
