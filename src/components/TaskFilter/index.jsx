20dcd929d63558e03643bd54dacdbff98a94d570
AliM-1997 <aliimortada797@gmail.com>
2024-09-04 13:08:12 +0300

    adding the tabel header for the filter task and initialize the first item ->date

--- src/components/TaskFilter/index.jsx
+++ src/components/TaskFilter/index.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 import "./style.css";
-const TaskFilter = () => {
-  return <div></div>;
+import Icon from "../../base/Icon";
+import { faCalendarAlt, faX } from "@fortawesome/free-solid-svg-icons";
+import ReactDate from "../../base/ReactDate";
+import Label from "../../base/Label";
+import Button from "../../base/Button";
+const TaskFilter = ({ datechange }) => {
+  return (
+    <div className="flex column taskFilter-wrapper gap end">
+      <Icon icon={faX} />
+      <table>
+        <tr>
+          <th>Date</th>
+          <th>Emploayee</th>
+          <th>Ticket</th>
+          <th>Status</th>
+        </tr>
+      </table>
+      <div className="flex column end gap">
+        <>
+          <ReactDate
+            leftIcon={faCalendarAlt}
+            required={false}
+            width="25vw"
+            name="Date"
+          />
+        </>
+
+        <Button
+          placeHolder="Submit"
+          textColor="white"
+          backgroundColor="primary"
+          onClick={datechange}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default TaskFilter;
