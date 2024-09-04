df0383ac9a675928e4e151675fed0e2867b9c9e7
AliM-1997 <aliimortada797@gmail.com>
2024-09-04 13:18:19 +0300

    adding the task filter header functionality

--- src/components/TaskFilter/index.jsx
+++ src/components/TaskFilter/index.jsx
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import Icon from "../../base/Icon";
 import { faCalendarAlt, faX } from "@fortawesome/free-solid-svg-icons";
 import ReactDate from "../../base/ReactDate";
-import Label from "../../base/Label";
 import Button from "../../base/Button";
 const TaskFilter = ({ datechange }) => {
+  const [choosenItem, setChoosenItem] = useState({
+    Date: false,
+    Emoloyee: false,
+    Ticket: false,
+    Status: false,
+  });
+  console.log(choosenItem);
+  const itemChange = (key, value) => {
+    setChoosenItem({
+      Date: false,
+      Employee: false,
+      Ticket: false,
+      Status: false,
+      [key]: value,
+    });
+  };
+
   return (
     <div className="flex column taskFilter-wrapper gap end">
       <Icon icon={faX} />
       <table>
         <tr>
-          <th>Date</th>
-          <th>Emploayee</th>
-          <th>Ticket</th>
-          <th>Status</th>
+          <th
+            className={choosenItem.Date ? "underline" : ""}
+            onClick={() => itemChange("Date", true)}
+          >
+            Date
+          </th>
+          <th
+            className={choosenItem.Emoloyee ? "underline" : ""}
+            onClick={() => itemChange("Employee", true)}
+          >
+            Employee
+          </th>
+          <th
+            className={choosenItem.Ticket ? "underline" : ""}
+            onClick={() => itemChange("Ticket", true)}
+          >
+            Ticket
+          </th>
+          <th
+            className={choosenItem.Status ? "underline" : ""}
+            onClick={() => itemChange("Status", true)}
+          >
+            Status
+          </th>
         </tr>
       </table>
       <div className="flex column end gap">
