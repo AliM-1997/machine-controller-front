5ee2a1b22731ac360e627e8e22d0a13ee2b0b807
AliM-1997 <aliimortada797@gmail.com>
2024-09-06 14:18:44 +0300

    adjusting the slider component

--- src/base/Slider/index.jsx
+++ src/base/Slider/index.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
-const Slider = () => {
-  return <div></div>;
+
+const Slider = ({ modeChange }) => {
+  const [isOn, setIsOn] = useState(false);
+
+  const handleToggle = () => {
+    setIsOn(!isOn);
+    modeChange();
+  };
+
+  return (
+    <div
+      className={`slider-container ${isOn ? "slider-on" : "slider-off"}`}
+      onClick={handleToggle}
+    >
+      <div className={`switch ${isOn ? "switch-on" : "switch-off"}`}></div>
+    </div>
+  );
 };
 
 export default Slider;
