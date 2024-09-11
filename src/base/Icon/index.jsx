228e676288bc8eeb9686a3f15eb22fb92751e039
AliM-1997 <aliimortada797@gmail.com>
2024-09-11 12:04:51 +0300

    adjusting icon props

--- src/base/Icon/index.jsx
+++ src/base/Icon/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Icon = ({ icon, color, onClick }) => {
+const Icon = ({ icon, color = "white", onClick }) => {
   const colorclass = `${color}-color`;
   return (
     <div>
