7f2dd01578cadeef16e2f49a59af1b4dc7f502a8
AliM-1997 <aliimortada797@gmail.com>
2024-09-11 12:14:32 +0300

    initializing the prediction  card  component

--- src/pages/Predictions/index.jsx
+++ src/pages/Predictions/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./style.css";
 import Header from "../../components/Header";
+import PredictionCard from "../../components/PredictionCard";
 const Predictions = () => {
   return (
     <div>
       <Header showChooseInput={false} pageName={"Predictions"} />
+      <div>
+        <PredictionCard />
+      </div>
     </div>
   );
 };
