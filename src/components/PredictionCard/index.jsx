de902962c4313d472e9727c64e469b9202e01a52
AliM-1997 <aliimortada797@gmail.com>
2024-09-11 12:27:19 +0300

    adding Statistic button to handle machine statistics

--- src/components/PredictionCard/index.jsx
+++ src/components/PredictionCard/index.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../../base/Button";
+import Papa from "papaparse";
 
-const PredictionCard = () => {
-  return <div>hello</div>;
+const PredictionCard = ({ id }) => {
+  const [csvData, setCsvData] = useState(null);
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      Papa.parse(file, {
+        complete: (results) => {
+          setCsvData(results.data);
+          console.log("Parsed CSV Data:", results.data);
+        },
+        header: true,
+      });
+    }
+  };
+
+  return (
+    <div>
+      <Button
+        placeHolder="Statistics"
+        backgroundColor="primary"
+        textColor="white"
+        onClick={() => document.getElementById("csvInput").click()}
+      />
+      <input
+        id="csvInput"
+        type="file"
+        accept=".csv"
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+    </div>
+  );
 };
 
 export default PredictionCard;
