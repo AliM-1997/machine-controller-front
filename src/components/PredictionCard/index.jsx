0c4a036029fa459157133a0490da353acca9f36f
AliM-1997 <aliimortada797@gmail.com>
2024-09-11 13:40:04 +0300

    adding the handler for create statistic and handle all spare part for the given machine

--- src/components/PredictionCard/index.jsx
+++ src/components/PredictionCard/index.jsx
@@ -1,37 +1,68 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../../base/Button";
 import Papa from "papaparse";
+import { Machines } from "../../data/remote/Machine";
+import { MachineStatistics } from "../../data/remote/machineStatistics";
 
-const PredictionCard = ({ id }) => {
+const PredictionCard = ({ serial_number }) => {
   const [csvData, setCsvData] = useState(null);
+  const [displaySparepart, setDisplaySparePart] = useState([]);
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
       Papa.parse(file, {
         complete: (results) => {
           setCsvData(results.data);
-          console.log("Parsed CSV Data:", results.data);
+          //   console.log("Parsed CSV Data:", results.data[0]);
         },
         header: true,
       });
     }
   };
-
+  const handleAllSparePart = async () => {
+    if (serial_number) {
+      try {
+        const data = await Machines.GetallSparepartForMachine(serial_number);
+        console.log(data);
+        setDisplaySparePart(data.spare_parts);
+      } catch (error) {
+        console.error("Error fetching all spare parts:", error.message);
+      }
+    }
+  };
+  const handleCreateStatistic = async () => {
+    if (csvData !== "") {
+      const response =
+        await MachineStatistics.CreateMachineStatisticCalculation(csvData);
+      console.log("from component", response);
+      setCsvData(null);
+    }
+  };
+  useEffect(() => {
+    if (csvData) {
+      handleCreateStatistic();
+    }
+  }, [csvData]);
+  useEffect(() => {
+    handleAllSparePart();
+  }, [serial_number]);
   return (
-    <div>
-      <Button
-        placeHolder="Statistics"
-        backgroundColor="primary"
-        textColor="white"
-        onClick={() => document.getElementById("csvInput").click()}
-      />
-      <input
-        id="csvInput"
-        type="file"
-        accept=".csv"
-        style={{ display: "none" }}
-        onChange={handleFileChange}
-      />
+    <div className="">
+      <div>
+        <Button
+          placeHolder="Statistics"
+          backgroundColor="primary"
+          textColor="white"
+          onClick={() => document.getElementById("csvInput").click()}
+        />
+        <input
+          id="csvInput"
+          type="file"
+          accept=".csv"
+          style={{ display: "none" }}
+          onChange={handleFileChange}
+        />
+      </div>
     </div>
   );
 };
--- src/pages/Predictions/index.jsx
+++ src/pages/Predictions/index.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import Header from "../../components/Header";
 import PredictionCard from "../../components/PredictionCard";
+import ChooseOption from "../../base/ChooseOption";
+import { useSelector } from "react-redux";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 const Predictions = () => {
+  const machine = useSelector((global) => global.data);
+  const [serialNumber, setSerialnumber] = useState("");
+  console.log(serialNumber);
+  console.log(machine);
+  const handleOptionSelect = (option) => {
+    setSerialnumber(option);
+  };
   return (
     <div>
       <Header showChooseInput={false} pageName={"Predictions"} />
       <div>
-        <PredictionCard />
+        <ChooseOption
+          name={"Machine "}
+          placeholder="Serial Number"
+          options={machine.MachineSerialNumber}
+          backgroundColor="white"
+          width="12vw"
+          leftIcon={faSearch}
+          iconColor="white"
+          required={false}
+          onSelect={(option) => handleOptionSelect(option.label)}
+        />
+        <PredictionCard serial_number={serialNumber} />
       </div>
     </div>
   );
