e361c2d8d4c95c6aa0268174573e099052cd91a8
AliM-1997 <aliimortada797@gmail.com>
2024-09-11 12:03:02 +0300

    adding addition funtion to remote file for spare part

--- src/data/remote/spareParts.js
+++ src/data/remote/spareParts.js
@@ -11,6 +11,14 @@ const SpareParts = {
       alert("No SparePart Found");
     }
   },
+  GetSparePartByID: async (id) => {
+    try {
+      const data = await requestApi({ route: `/sparePart/${id}` });
+      return data;
+    } catch (error) {
+      console.error("Error creating user:", error.message);
+    }
+  },
   GetByType: async (type) => {
     try {
       const data = await requestApi({ route: `/sparePart/type/${type}` });
@@ -27,6 +35,71 @@ const SpareParts = {
     } catch (error) {
       console.error("Error creating user:", error.message);
       alert("No SparePart Found ");
+    }
+  },
+  DeleteSparePartImage: async (id) => {
+    try {
+      const data = await requestApi({
+        route: `/sparePart/deleteImage/${id}`,
+        requestMethod: RequestMethods.DELETE,
+      });
+      return data;
+    } catch (error) {
+      console.error("Error creating user:", error.message);
+      alert("Cannot Delete Image");
+    }
+  },
+
+  UploadImage: async (file, id) => {
+    try {
+      const formData = new FormData();
+      formData.append("image", file);
+      const data = await requestApi({
+        route: `/sparePart/uploadImage/${id}`,
+        requestMethod: RequestMethods.POST,
+        body: formData,
+      });
+      return data;
+    } catch (error) {
+      console.error("Error creating user:", error.message);
+    }
+  },
+  DeleteSparePart: async (id) => {
+    try {
+      const data = await requestApi({
+        route: `/sparePart/${id}`,
+        requestMethod: RequestMethods.DELETE,
+      });
+      return data;
+    } catch (error) {
+      console.error("Error creating user:", error.message);
+      alert("SparePart not Found ");
+    }
+  },
+  CreateSparePart: async (form) => {
+    try {
+      const data = await requestApi({
+        route: `/sparePart`,
+        requestMethod: RequestMethods.POST,
+        body: form,
+      });
+      return data;
+    } catch (error) {
+      console.error("Error creating user:", error.message);
+      alert("Spare Part Not Created ");
+    }
+  },
+  UpdateSparePart: async (id, form) => {
+    try {
+      const data = await requestApi({
+        route: `/sparePart/${id}`,
+        requestMethod: RequestMethods.PATCH,
+        body: form,
+      });
+      return data;
+    } catch (error) {
+      console.error("Error creating user:", error.message);
+      alert("Cannot Update Try again later");
     }
   },
 };
