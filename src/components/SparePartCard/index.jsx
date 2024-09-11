b82e8d0cf820bdb9f687e92c525a440356524dc2
AliM-1997 <aliimortada797@gmail.com>
2024-09-11 12:07:15 +0300

    handling sopre part card functionalities

--- src/components/SparePartCard/index.jsx
+++ src/components/SparePartCard/index.jsx
@@ -1,19 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.css";
 import DisplayImage from "../../base/DisplayImage";
 import Label from "../../base/Label";
-const SparePartCard = ({ sparePart }) => {
+import { useDarkMode } from "../../data/constext/DarkModeContext";
+import Icon from "../../base/Icon";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import Gear from "../../assets/images/gear.jpg";
+const SparePartCard = ({ sparePart, onEdit }) => {
+  const { darkMode } = useDarkMode();
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      darkMode ? "dark" : "light"
+    );
+  }, [darkMode]);
   return (
     <div className="flex column white-bg sparepart-card">
-      <DisplayImage
-        url={sparePart.mage_path}
-        width="150px"
-        height="150px"
-        borderRadius="12px  12px 0 0"
-      />
-      <div className=" flex column center">
-        <Label placeholder={sparePart.name} backgroundColor="white" />
-        <Label placeholder={sparePart.serial_number} backgroundColor="white" />
+      {sparePart.image_path ? (
+        <DisplayImage
+          url={sparePart.image_path}
+          width="150px"
+          height="150px"
+          borderRadius="12px  12px 0 0"
+        />
+      ) : (
+        <img
+          src={Gear}
+          style={{
+            width: "150px",
+            height: "150px",
+            borderRadius: "12px 12px 0 0",
+          }}
+        />
+      )}
+      <div className="flex column sparepart-info">
+        <div className=" flex   space-btw   ">
+          <Label
+            placeholder={sparePart.name}
+            backgroundColor="mode"
+            textColor="mode"
+          />
+          <Icon
+            icon={faEdit}
+            color={darkMode ? "white" : "black"}
+            onClick={onEdit}
+          />
+        </div>
+        <Label
+          placeholder={sparePart.serial_number}
+          backgroundColor="mode"
+          textColor="mode"
+        />
       </div>
     </div>
   );
