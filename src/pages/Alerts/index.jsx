2ab7f0329707e2ba6cfd10ee650a4bf573c3ac37
AliM-1997 <aliimortada797@gmail.com>
2024-09-16 12:32:04 +0300

    adding channel to alerts page

--- src/pages/Alerts/index.jsx
+++ src/pages/Alerts/index.jsx
@@ -4,33 +4,55 @@ import Header from "../../components/Header";
 import { Notifications } from "../../data/remote/notification";
 import Button from "../../base/Button";
 import { useDarkMode } from "../../data/constext/DarkModeContext";
+import echo from "../../utils/echo";
+
 const Alerts = () => {
   const { darkMode } = useDarkMode();
   const [nonRead, setNoneRead] = useState([]);
+
   const handleUnReadNotification = async () => {
-    const response = await Notifications.UnReadNotification();
-    console.log("unread notifications in alerts", response);
-    setNoneRead(response.notifications || []);
+    try {
+      const response = await Notifications.UnReadNotification();
+      console.log("unread notifications in alerts", response);
+      setNoneRead(response.notifications || []);
+    } catch (error) {
+      console.error("Error fetching unread notifications:", error);
+    }
   };
 
   const handleMarkNotificationAsRead = async (notificationId) => {
-    const response = await Notifications.MarkNottificationAsRead(
-      notificationId
-    );
-    setNoneRead(
-      nonRead.filter((notification) => notification.id !== notificationId)
-    );
+    try {
+      await Notifications.MarkNottificationAsRead(notificationId);
+      setNoneRead(
+        nonRead.filter((notification) => notification.id !== notificationId)
+      );
+    } catch (error) {
+      console.error("Error marking notification as read:", error);
+    }
   };
 
   useEffect(() => {
     handleUnReadNotification();
+
+    const channel = echo.channel("notifications");
+    channel.listen(".NotificationCountEvent", (event) => {
+      console.log("New Notification:", event.notification);
+      setNoneRead((prevNotifications) => [
+        event.notification,
+        ...prevNotifications,
+      ]);
+    });
+
+    return () => {
+      channel.stopListening(".NotificationCountEvent");
+    };
   }, []);
 
   return (
     <div>
       <Header
         pageName={"Alerts"}
-        showChooseInput
+        showChooseInput={false}
         backgroundColor_btn={darkMode ? "black" : "white"}
         border={false}
         textColor_btn={darkMode ? "black" : "white"}
@@ -52,7 +74,7 @@ const Alerts = () => {
             darkMode ? "black-bg" : "white-bg"
           } notification-box`}
         >
-          <div className="flex column  gap full-width notification-info">
+          <div className="flex column gap full-width notification-info">
             {nonRead.length > 0 ? (
               nonRead.map((alert) => (
                 <div
@@ -70,7 +92,6 @@ const Alerts = () => {
                   <p className={darkMode ? "white-txt" : "black-txt"}>
                     {alert.data.status}
                   </p>
-
                   <div>
                     <Button
                       placeHolder="mark as read"
