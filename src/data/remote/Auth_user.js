f66cc6be81546d71a380d3a1163aa7c9e9916716
AliM-1997 <aliimortada797@gmail.com>
2024-09-19 10:53:05 +0300

    handling toast error in auth remote fle

--- src/data/remote/Auth_user.js
+++ src/data/remote/Auth_user.js
@@ -1,40 +1,51 @@
+import { toast } from "react-toastify";
 import { requestApi } from "../../utils/request";
 import { RequestMethods } from "../../utils/request_methods";
 import { authLocal } from "../local/Auth_local";
 export const authRemote = {
   Login: async (email, password) => {
-    const data = await requestApi({
-      route: "/login",
-      requestMethod: RequestMethods.POST,
-      body: {
-        email,
-        password,
-      },
-    });
-    return data;
+    try {
+      const data = await requestApi({
+        route: "/login",
+        requestMethod: RequestMethods.POST,
+        body: {
+          email,
+          password,
+        },
+      });
+      return data;
+    } catch (error) {
+      toast.error("Error logging in: " + error.message);
+    }
   },
+
   Signup: async (email, password, name) => {
-    const data = await requestApi({
-      route: "/register",
-      requestMethod: RequestMethods.POST,
-      body: {
-        name,
-        email,
-        password,
-      },
-    });
-    return data;
+    try {
+      const data = await requestApi({
+        route: "/register",
+        requestMethod: RequestMethods.POST,
+        body: {
+          name,
+          email,
+          password,
+        },
+      });
+      return data;
+    } catch (error) {
+      toast.error("Error during Sign Up: " + error.message);
+    }
   },
+
   Logout: async () => {
     try {
       const data = await requestApi({
         route: "/logout",
         requestMethod: RequestMethods.POST,
       });
-      authLocal.saveToken(null);
-      console.log("Logout successful:", data);
+      authLocal.saveToken(null); // Remove token on logout
+      toast.success("Logged out successfully");
     } catch (error) {
-      // throw error;
+      toast.error("Error logging out: " + error.message);
     }
   },
 };
