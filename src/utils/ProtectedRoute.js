b8461007b390e076e978609236c052764cc2514d
AliM-1997 <aliimortada797@gmail.com>
2024-09-25 22:27:49 +0300

    adhusting the protected route and checking role

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "../data/constext/authContext";
 
-const ProtectedRoute = () => {
-  const token = localStorage.getItem("token");
+const ProtectedRoute = ({ requiredRole }) => {
+  const { isAuthenticated, user, loading } = useContext(AuthContext);
 
-  if (!token) {
-    return <Navigate to="/login" replace />;
+  console.log(
+    "isAuthenticated:",
+    isAuthenticated,
+    "user:",
+    user,
+    "loading:",
+    loading,
+    "requiredRole:",
+    requiredRole
+  );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  if (requiredRole && user?.role !== requiredRole) {
+    return <Navigate to="/unauthorized" />;
   }
 
   return <Outlet />;
