c0cce97764730cf9ca99cdcb7ceb7c248f33e41e
AliM-1997 <aliimortada797@gmail.com>
2024-09-25 17:08:38 +0300

    adding the website icon and title

--- src/base/DisplayImage/index.jsx
+++ src/base/DisplayImage/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import white_bg from "../../assets/images/white-bg.png";
+
 const DisplayImage = ({
   url,
   width = "",
@@ -8,10 +9,17 @@ const DisplayImage = ({
   borderRadius = "50%",
 }) => {
   const baseUrl = process.env.REACT_APP_BASE_URL_IMAGE;
+  // Construct the correct URL, without "/app/public"
   const fullUrl = url ? `${baseUrl}/storage/${url}` : white_bg;
+
+  console.log("fullurl", fullUrl);
+  console.log("url", url);
+  console.log("baseurl", baseUrl);
+
   const handleError = (e) => {
     e.target.src = white_bg;
   };
+
   return (
     <div>
       <img
